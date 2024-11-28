22e63094cc154479a3de68402c4d1c92f97339ff
Luca Mondada <luca@mondada.net>
2024-11-28 09:47:55 +0100

    support varying rem sizes

--- static/js/footnotes.js
+++ static/js/footnotes.js
@@ -1,9 +1,16 @@
+function getRemInPixels() {
+    // Get the computed style of the root element (html) to determine the root font size
+    const rootFontSize = getComputedStyle(document.documentElement).fontSize;
+    return parseFloat(rootFontSize); // Convert '16px' to 16 (number)
+}
+
 function toggleFloatingFootnotes() {
     const footnotes = document.querySelectorAll('div.footnotes');
+    const remInPixels = getRemInPixels();
     const screenWidth = window.innerWidth;
 
     footnotes.forEach(footnote => {
-        if (screenWidth > 56 * 16) { // 56rem converted to pixels (assuming 1rem = 16px)
+        if (screenWidth > 56 * remInPixels) { // 56rem: width set by hugo-book
             footnote.classList.add('floating-footnotes');
         } else {
             footnote.classList.remove('floating-footnotes');
